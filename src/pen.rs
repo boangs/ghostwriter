f0f2234023ef5a3178974ee70c5e087955b2006f
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-02-16 17:44:44 +0800

    书写测试

--- src/pen.rs
+++ src/pen.rs
@@ -5,52 +5,54 @@ use std::io::Write;
 use std::os::unix::fs::OpenOptionsExt;
 use nix::libc;
 use std::os::unix::io::AsRawFd;
+use std::fs::File;
+use std::io::{Read, Write};
 
 pub struct Pen {
     no_draw: bool,
-    display_device: Option<std::fs::File>,
+    display_device: Option<File>,
+    pen_device: Option<File>,
     width: u32,
     height: u32,
     buffer: Vec<u8>,
+    is_drawing: bool,
+    last_y: i32,
 }
 
 impl Pen {
     pub fn new(no_draw: bool) -> Self {
-        let (display_device, width, height) = if !no_draw {
-            // 使用 reMarkable 的 framebuffer 设备
-            let device_path = "/dev/fb0";
-            println!("尝试打开显示设备: {}", device_path);
-            match OpenOptions::new()
+        let (display_device, pen_device, width, height) = if !no_draw {
+            // 打开显示设备
+            let display = File::options()
                 .read(true)
                 .write(true)
                 .custom_flags(libc::O_RDWR)
-                .open(device_path) 
-            {
-                Ok(device) => {
-                    println!("成功打开显示设备: {}", device_path);
-                    let fd = device.as_raw_fd();
-                    println!("显示设备文件描述符: {}", fd);
-                    (Some(device), 2832, 2064)  // reMarkable Paper Pro 分辨率
-                },
-                Err(e) => {
-                    println!("打开显示设备失败: {}", e);
-                    (None, 0, 0)
-                }
-            }
+                .open("/dev/fb0")
+                .ok();
+
+            // 打开手写笔输入设备
+            let pen = File::options()
+                .read(true)
+                .open("/dev/input/event2")  // Elan marker input
+                .ok();
+
+            (display, pen, 2832, 2064)  // reMarkable Paper Pro 分辨率
         } else {
-            (None, 0, 0)
+            (None, None, 0, 0)
         };
 
-        let buffer_size = (width * height) as usize;  // 8位灰度
-        println!("创建显示缓冲区，大小: {} 字节", buffer_size);
-        let buffer = vec![0xFF; buffer_size];
+        let buffer_size = (width * height) as usize;
+        let buffer = vec![255u8; buffer_size];  // 白色背景
 
         Self {
             no_draw,
             display_device,
+            pen_device,
             width,
-            height,
+            height, 
             buffer,
+            is_drawing: false,
+            last_y: 0,
         }
     }
 
@@ -85,7 +87,47 @@ impl Pen {
         // 清理资源
     }
 
+    pub fn handle_pen_input(&mut self) -> Result<()> {
+        if let Some(pen_device) = &mut self.pen_device {
+            let mut event_buffer = [0u8; 24];  // Linux input_event 结构体大小
+            
+            while let Ok(_) = pen_device.read_exact(&mut event_buffer) {
+                let event = parse_input_event(&event_buffer);
+                
+                match event.type_ {
+                    0 => {}, // EV_SYN
+                    3 => {   // EV_ABS
+                        match event.code {
+                            0 => {  // ABS_X
+                                let x = event.value as i32;
+                                let y = self.last_y;
+                                self.draw_pixel(x, y);
+                            },
+                            1 => {  // ABS_Y 
+                                self.last_y = event.value as i32;
+                            },
+                            24 => { // ABS_PRESSURE
+                                if event.value > 0 {
+                                    self.is_drawing = true;
+                                } else {
+                                    self.is_drawing = false;
+                                }
+                            },
+                            _ => {}
+                        }
+                    },
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    }
+
     fn draw_pixel(&mut self, x: i32, y: i32) {
+        if !self.is_drawing {
+            return;
+        }
+        
         if x < 0 || y < 0 || x >= self.width as i32 || y >= self.height as i32 {
             return;
         }
@@ -94,6 +136,8 @@ impl Pen {
         if offset < self.buffer.len() {
             self.buffer[offset] = 0;  // 黑色像素
         }
+        
+        self.flush().unwrap_or_default();
     }
 
     pub fn draw_bitmap(&mut self, bitmap: &Vec<Vec<bool>>) -> Result<()> {
@@ -109,20 +153,8 @@ impl Pen {
 
     pub fn flush(&mut self) -> Result<()> {
         if let Some(device) = &mut self.display_device {
-            println!("开始刷新显示");
-            println!("缓冲区大小: {} 字节", self.buffer.len());
-            
-            // 写入 framebuffer
-            match device.write_all(&self.buffer) {
-                Ok(_) => println!("写入缓冲区成功"),
-                Err(e) => println!("写入缓冲区失败: {}", e)
-            }
-            
-            // 刷新屏幕
-            match device.flush() {
-                Ok(_) => println!("刷新显示成功"),
-                Err(e) => println!("刷新显示失败: {}", e)
-            }
+            device.write_all(&self.buffer)?;
+            device.flush()?;
         }
         Ok(())
     }
@@ -131,5 +163,32 @@ impl Pen {
 impl Drop for Pen {
     fn drop(&mut self) {
         self.cleanup();
+    }
+}
+
+#[derive(Debug)]
+struct InputEvent {
+    time: libc::timeval,
+    type_: u16,
+    code: u16,
+    value: i32,
+}
+
+fn parse_input_event(buffer: &[u8]) -> InputEvent {
+    // 解析 Linux input_event 结构体
+    let time_sec = u64::from_ne_bytes(buffer[0..8].try_into().unwrap());
+    let time_usec = u64::from_ne_bytes(buffer[8..16].try_into().unwrap());
+    let type_ = u16::from_ne_bytes(buffer[16..18].try_into().unwrap());
+    let code = u16::from_ne_bytes(buffer[18..20].try_into().unwrap());
+    let value = i32::from_ne_bytes(buffer[20..24].try_into().unwrap());
+
+    InputEvent {
+        time: libc::timeval {
+            tv_sec: time_sec as i64,
+            tv_usec: time_usec as i64,
+        },
+        type_,
+        code,
+        value,
     }
 }
