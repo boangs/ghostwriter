01ad4afab72364decfed4364db8814f8ead36153
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-02-18 16:58:55 +0800

    ip 明确

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Set the hostname for the remarkable based on $1 and fall back to `remarkable`
-remarkable="${1:-remarkable}"
+remarkable="10.11.99.1"
 
 if [ "$1" == "local" ]; then
     cargo build --release
 else
-    cross build \
-      --release \
-      --target=armv7-unknown-linux-gnueabihf \
-      && scp target/armv7-unknown-linux-gnueabihf/release/ghostwriter root@$remarkable:
+    # export PKG_CONFIG_SYSROOT_DIR="<toolchain_install_path>/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi"
+    # export PKG_CONFIG_PATH="<toolchain_install_path>/sysroots/cortexa7hf-neon-remarkable-linux-gnueabi/usr/lib/pkgconfig"
+    # export PKG_CONFIG_ALLOW_CROSS=1
+    cross build --release --target=aarch64-unknown-linux-gnu && scp target/aarch64-unknown-linux-gnu/release/ghostwriter root@$remarkable:
 fi
-
