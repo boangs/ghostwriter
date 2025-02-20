cce2f8fd719929e145d3782787388d40715bad5f
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-02-20 17:43:09 +0800

    数据格式

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 // remarkable paper pro 实际分辨率
-pub const REMARKABLE_WIDTH: i32 = 1620;
-pub const REMARKABLE_HEIGHT: i32 = 2160;
+pub const REMARKABLE_WIDTH: u32 = 1620;
+pub const REMARKABLE_HEIGHT: u32 = 2160;
 
 // 输入设备坐标系
 pub const INPUT_WIDTH: i32 = 15725;
