d5490ca13232fb756eff43e28b87116f7b0e39e6
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-02-20 23:40:54 +0800

    优化

--- src/font.rs
+++ src/font.rs
@@ -14,16 +14,14 @@ impl FontRenderer {
             .expect("Failed to load font")
             .data;
         
-        // 修复 Rc<Vec<u8>> 的问题
         let font_data = Rc::new(font_data.to_vec());
         let face = lib.new_memory_face(font_data, 0)?;
-        face.set_pixel_sizes(0, 100)?; // 设置字体大小
+        face.set_pixel_sizes(0, 100)?;
         
         Ok(FontRenderer { face })
     }
 
     pub fn get_char_strokes(&self, c: char, size: f32) -> Result<Vec<Vec<(i32, i32)>>> {
-        // 设置字体大小
         self.face.set_pixel_sizes(0, size as u32)?;
         self.face.load_char(c as usize, freetype::face::LoadFlag::NO_SCALE)?;
         
@@ -31,22 +29,21 @@ impl FontRenderer {
         let outline = self.face.glyph().outline().unwrap();
         
         // 获取轮廓点
-        let mut points = outline.points().iter()
+        let points: Vec<_> = outline.points().iter()
             .map(|p| (p.x as i32, p.y as i32))
-            .collect::<Vec<_>>();
+            .collect();
             
         // 根据轮廓标记分割笔画
         let mut current_stroke = Vec::new();
-        let tags = outline.tags();
         let contours = outline.contours();
         
         let mut point_index = 0;
-        for contour_end in contours {
-            while point_index <= contour_end as usize {
+        for &end_index in contours {
+            while point_index <= *end_index as usize {
                 let point = points[point_index];
                 current_stroke.push(point);
                 
-                if point_index == contour_end as usize {
+                if point_index == *end_index as usize {
                     // 闭合轮廓
                     if !current_stroke.is_empty() {
                         current_stroke.push(current_stroke[0]);
