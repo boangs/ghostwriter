631b9a5b1083d9db25ca435238d8ff2acfaf05b6
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-02-23 22:44:21 +0800

    获取填充轮廓

--- src/font.rs
+++ src/font.rs
@@ -24,44 +24,45 @@ impl FontRenderer {
         self.face.set_pixel_sizes(0, size as u32)?;
         self.face.load_char(
             c as usize, 
-            freetype::face::LoadFlag::DEFAULT
+            freetype::face::LoadFlag::RENDER | freetype::face::LoadFlag::MONOCHROME
         )?;
         
         let glyph = self.face.glyph();
-        let outline = glyph.outline().ok_or_else(|| anyhow::anyhow!("无法获取字符轮廓"))?;
-        
-        let points = outline.points();
-        let tags = outline.tags();
-        let contours = outline.contours();
+        let bitmap = glyph.bitmap();
+        let width = bitmap.width() as usize;
+        let height = bitmap.rows() as usize;
+        let buffer = bitmap.buffer();
         
         let mut strokes = Vec::new();
-        let mut start: usize = 0;
+        let mut current_stroke = Vec::new();
+        let scale = 1.0;
         
-        let scale = 0.02;
-        
-        for end in contours.iter() {
-            let mut current_stroke = Vec::new();
-            let end_idx = *end as usize;
-            
-            for i in start..=end_idx {
-                let point = points[i];
-                let tag = tags[i];
+        for y in 0..height {
+            let mut in_stroke = false;
+            for x in 0..width {
+                let byte = buffer[y * bitmap.pitch() as usize + (x >> 3)];
+                let bit = (byte >> (7 - (x & 7))) & 1;
                 
-                if tag & 0x1 != 0 {
-                    let x = (point.x as f32 * scale) as i32;
-                    let y = -(point.y as f32 * scale) as i32;
-                    current_stroke.push((x, y));
+                if bit == 1 {
+                    if !in_stroke {
+                        // 开始新的笔画
+                        if !current_stroke.is_empty() {
+                            strokes.push(current_stroke);
+                            current_stroke = Vec::new();
+                        }
+                        in_stroke = true;
+                    }
+                    let px = (x as f32 * scale) as i32;
+                    let py = (y as f32 * scale) as i32;
+                    current_stroke.push((px, py));
+                } else if in_stroke {
+                    in_stroke = false;
                 }
             }
-            
-            if !current_stroke.is_empty() {
-                if current_stroke[0] != *current_stroke.last().unwrap() {
-                    current_stroke.push(current_stroke[0]);
-                }
-                strokes.push(current_stroke);
-            }
-            
-            start = end_idx + 1;
+        }
+        
+        if !current_stroke.is_empty() {
+            strokes.push(current_stroke);
         }
         
         Ok(strokes)
