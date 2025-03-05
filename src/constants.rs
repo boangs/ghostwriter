975b8aa54c7b3b13ba7bc0cd545da89f8e3180c5
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-03-05 22:51:45 +0800

    坐标变更

--- src/constants.rs
+++ src/constants.rs
@@ -7,8 +7,8 @@ pub const INPUT_WIDTH: i32 = 15725;
 pub const INPUT_HEIGHT: i32 = 20967;
 
 // 触摸设备坐标系
-pub const TOUCH_WIDTH: i32 = 1620;
-pub const TOUCH_HEIGHT: i32 = 2160;
+pub const TOUCH_WIDTH: i32 = 2064;
+pub const TOUCH_HEIGHT: i32 = 2832;
 
 // 截图尺寸
 pub const SCREENSHOT_WIDTH: i32 = 1620;
