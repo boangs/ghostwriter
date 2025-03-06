b27a52c670adea5c14b428201f0717b5641cbc72
xurx@si-tech.com.cn <xurx@si-tech.com.cn>
2025-03-06 23:23:58 +0800

    恢复

--- src/llm_engine/openai.rs
+++ src/llm_engine/openai.rs
@@ -40,7 +40,7 @@ impl OpenAI {
 
 impl LLMEngine for OpenAI {
     fn new(options: &OptionMap) -> Self {
-        let api_key = option_or_env(&options, "api_key", "OPENAI_API_KEY" ,"Bearer");
+        let api_key = option_or_env(&options, "api_key", "OPENAI_API_KEY");
         let base_url = option_or_env_fallback(
             &options,
             "base_url",
